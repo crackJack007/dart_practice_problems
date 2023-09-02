4ffe548b260c18ed3ea0ee4780b287cafa2c8c76
Anantha Subramanya <Anantha.subramanya@milvik.se>
2023-09-02 17:50:30 +0530

    Initial Commit

---
+++ bin/dart_practice_problems.dart
@@ -0,0 +1,5 @@
+import 'package:dart_practice_problems/dart_practice_problems.dart' as dart_practice_problems;
+
+void main(List<String> arguments) {
+  print('Hello world: ${dart_practice_problems.calculate()}!');
+}
---
+++ test/dart_practice_problems_test.dart
@@ -0,0 +1,8 @@
+import 'package:dart_practice_problems/dart_practice_problems.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
